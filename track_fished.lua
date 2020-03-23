7ea73e6f4ef2f5b001e52901631e1aed09ea455b
Khrash <francois.alleyn@hotmail.com>
2020-03-23 12:30:36 +0100

    creation of fish table

--- track_fished.lua
+++ track_fished.lua
@@ -1,13 +1,20 @@
 --Track_Fished is meant to register events of anything that is Fished and log its info
 
 local ItemPushedFrame = CreateFrame("Frame")
-
+local peche = {}
 local function LogLoot()
     if( IsFishingLoot() ) then
         local itemLink = GetLootSlotLink(1)
         local itemName = GetItemInfo(itemLink)
-        SendChatMessage("Jai trouvé un " .. itemName ,"GUILD" , DEFAULT_CHAT_FRAME.editBox.languageID);
-     end
+        local fish = peche[itemLink] 
+        if fish = nil then
+            peche[itemLink] = 1
+        else 
+            fish = fish + 1
+            peche[itemLink] = fish
+        end
+        SendChatMessage("Jai trouvé un " .. itemName .." c'est le "..fish .." de la journée!","GUILD" , DEFAULT_CHAT_FRAME.editBox.languageID);
+    end
 end
 
 ItemPushedFrame:SetScript("OnEvent", LogLoot)
