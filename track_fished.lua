417a5fd720a17ec5a6be3fa94320479ded9a1416
pmeves <pedromeves@gmail.com>
2020-03-23 13:05:38 +0100

    fished info more complete now

--- track_fished.lua
+++ track_fished.lua
@@ -6,14 +6,25 @@ local function LogLoot()
     if( IsFishingLoot() ) then
         local itemLink = GetLootSlotLink(1)
         local itemName = GetItemInfo(itemLink)
-        local fish = peche[itemLink] 
+        local fish = peche[itemLink]
+
         if fish == nil then
-            peche[itemLink] = 1
+            fish = {}
+            fish.ItemLink = itemLink
+            fish.ItemName = itemName
+            fish.TotalFishedCount = 1
+            fish.FirstFishedDate = date()
+            fish.LastFishedDate = date()
+            peche[itemLink] = fish
         else 
-            fish = fish + 1
+            fish.TotalFishedCount = fish.TotalFishedCount + 1
+            fish.LastFishedDate = date()
             peche[itemLink] = fish
         end
-        SendChatMessage("Jai trouvé un " .. itemName .." c'est le "..fish .." de la journée!","GUILD" , DEFAULT_CHAT_FRAME.editBox.languageID);
+
+        SendChatMessage("Jai trouvé un " .. itemName .." c'est le "..fish.TotalFishedCount .." de la journée!","GUILD" , DEFAULT_CHAT_FRAME.editBox.languageID);
+        SendChatMessage("La premiere fois que jai trouvé ce poisson cetait en " .. tostring(fish.FirstFishedDate),"GUILD" , DEFAULT_CHAT_FRAME.editBox.languageID);
+
     end
 end
 
